93e3dba4436126f959d78111dd671f3ae3b339c3
Arijit Das <arijitad.in@gmail.com>
2022-01-18 23:08:11 +0530

    Code cleanup.

--- pkg/validator/config.go
+++ pkg/validator/config.go
@@ -10,7 +10,22 @@ import (
 	"github.com/spf13/viper"
 )
 
-var testChainConfig = &params.ChainConfig{ChainID: big.NewInt(1), HomesteadBlock: big.NewInt(0), EIP150Block: big.NewInt(0), EIP155Block: big.NewInt(0), EIP158Block: big.NewInt(0), ByzantiumBlock: big.NewInt(0), ConstantinopleBlock: big.NewInt(0), PetersburgBlock: big.NewInt(0), IstanbulBlock: big.NewInt(0), MuirGlacierBlock: big.NewInt(0), BerlinBlock: big.NewInt(0), LondonBlock: big.NewInt(100), ArrowGlacierBlock: big.NewInt(0), Ethash: new(params.EthashConfig)}
+var testChainConfig = &params.ChainConfig{
+	ChainID:             big.NewInt(1),
+	HomesteadBlock:      big.NewInt(0),
+	EIP150Block:         big.NewInt(0),
+	EIP155Block:         big.NewInt(0),
+	EIP158Block:         big.NewInt(0),
+	ByzantiumBlock:      big.NewInt(0),
+	ConstantinopleBlock: big.NewInt(0),
+	PetersburgBlock:     big.NewInt(0),
+	IstanbulBlock:       big.NewInt(0),
+	MuirGlacierBlock:    big.NewInt(0),
+	BerlinBlock:         big.NewInt(0),
+	LondonBlock:         big.NewInt(100),
+	ArrowGlacierBlock:   big.NewInt(0),
+	Ethash:              new(params.EthashConfig),
+}
 
 type Config struct {
 	dbParams postgres.ConnectionParams
