a56da1782a9d372d70bd61ee4e4cb5dad6b1af6a
prathamesh0 <prathamesh.musale0@gmail.com>
2022-05-23 19:43:09 +0530

    Update instructions to run tests

--- scripts/run_unit_test.sh
+++ scripts/run_unit_test.sh
@@ -1,9 +1,17 @@
+#!/bin/bash
+
+set -e
+
 # Clear up existing docker images and volume.
 docker-compose down --remove-orphans --volumes
 
-docker-compose -f docker-compose.yml up -d ipld-eth-db
-sleep 10
+# Spin up TimescaleDB
+docker-compose -f docker-compose.yml up -d migrations ipld-eth-db
+sleep 45
 
+# Run unit tests
+go clean -testcache
 PGPASSWORD=password DATABASE_USER=vdbm DATABASE_PORT=8077 DATABASE_PASSWORD=password DATABASE_HOSTNAME=127.0.0.1 DATABASE_NAME=vulcanize_testing make test
 
+# Clean up
 docker-compose down --remove-orphans --volumes
