47f35c35d1a5da860028cb42774c88f1676fe960
Roy Crihfield <roy@manteia.ltd>
2023-05-27 22:45:14 +0800

    wait for block 2

--- scripts/get-block-number.sh
+++ scripts/get-block-number.sh
@@ -5,7 +5,7 @@ set -eu
 geth_endpoint="$1"
 
 latest_block_hex=$(curl -s $geth_endpoint -X POST -H "Content-Type: application/json" \
-    --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":83}' | \
+    --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":42}' | \
     jq -r .result)
 
 printf "%d" $latest_block_hex
